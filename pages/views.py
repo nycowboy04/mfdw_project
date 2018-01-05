4ac5641048a3ee4bc67ee69d3918295e09d1ba41
Matthew Sheinberg <msheinberg@gmail.com>
2018-01-05 07:03:22 -0500

    in middle of chapter 9

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from . models import Page
 
-def index (request):
-#    return HttpResponse("<h1>The Meandco Homepage</h1>")
-    return render(request, "pages/page.html")
+def index (request, pagename):
+    pagename = '/' + pagename
+    pg=Page.objects.get(permalink=pagename)
+    context ={
+        'title':pg.title,
+        'content':pg.bodytext,
+        'last_updated': pg.update_date,
+    }
+    assert False
+    return render(request, 'pages/page.html', context)
