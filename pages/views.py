36a3a74261ecf86fa7f4f3c9a7c59367e38aeb7e
Matthew Sheinberg <msheinberg@gmail.com>
2018-01-08 12:17:59 -0500

    in the middle of chapter 9

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # from django.http import HttpResponse
 from . models import Page
 
 def index (request, pagename):
     pagename = '/' + pagename
-    pg=Page.objects.get(permalink=pagename)
+    pg= get_object_or_404(Page, permalink=pagename)
     context ={
         'title' : pg.title,
         'content' : pg.bodytext,
         'last_updated' : pg.update_date,
+        'page_list' : Page.objects.all(),
     }
     #assert False
     return render(request, 'pages/page.html', context)
