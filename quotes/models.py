bdd6804e485ab99bb155d91fc56294f096c8d102
Matthew Sheinberg <msheinberg@gmail.com>
2018-01-09 10:40:11 -0500

    Fixed a few type-os in models.py and settings.py. Then we made the migrations for the development server

--- quotes/models.py
+++ quotes/models.py
@@ -8,11 +8,11 @@ STATUS_CHOICES = (
 
 PRIORITY_CHOICES = (
     ('U', 'Urgent - 1 week or less'),
-    ('N', 'Normal - 2 to 4 weeks')
+    ('N', 'Normal - 2 to 4 weeks'),
     ('L', 'Low - Still Researching'),
 )
 
-class Quote(models. Model):
+class Quote(models.Model):
     name = models.CharField(max_length = 100)
     position = models.CharField(max_length = 60, blank = True)
     company = models.CharField(max_length = 60, blank = True)
@@ -23,7 +23,7 @@ class Quote(models. Model):
     description = models.TextField()
     sitestatus = models.CharField(max_length=20, choices=STATUS_CHOICES)
     priority = models.CharField(max_length = 40, choices = PRIORITY_CHOICES)
-    jobfile = models.FileField(updload_to='uploads/', blank=True)
+    jobfile = models.FileField(upload_to='uploads/', blank=True)
     submitted = models.DateField(auto_now_add=True)
     quotedate = models.DateField(blank=True, null=True)
     quoteprice=models.DecimalField(decimal_places=2, max_digits=7, blank=True,
