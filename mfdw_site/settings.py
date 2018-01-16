c4e3d87251693988970cbc81cdbfabe40ec16038
Matthew Sheinberg <msheinberg@gmail.com>
2018-01-16 13:22:16 -0500

    installed quotes app, and created the quotes model

--- mfdw_site/settings.py
+++ mfdw_site/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'quotes.apps.QuotesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
