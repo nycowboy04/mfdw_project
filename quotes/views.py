f4ead910e7316262dc94911ef668dbcdd644a52a
Matthew Sheinberg <msheinberg@gmail.com>
2018-01-17 17:13:14 -0500

    Implemented a user authentication. Need to test the registration component.

--- quotes/views.py
+++ quotes/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import Quote
 from .forms import QuoteForm
 from pages.models import Page
 
-class QuoteList(ListView):
+class Register(CreateView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('registration-success')
+
+    def form_valid(self, form):
+        form.save()
+        return HttpResponseRedirect(self.success_url)
+
+class QuoteList(LoginRequiredMixin, ListView):
+    login_url= reverse_lazy('login')
     model = Quote
     context_object_name = 'all_quotes'
+    def get_query_set(self):
+        return Quote.objects.filter(username=self.request.user)
+
     def get_context_data(self, **kwargs):
         context=super(QuoteList, self).get_context_data(**kwargs)
         context['page_list']=Page.objects.all()
@@ -21,13 +39,19 @@ class QuoteView(DetailView):
         context = super(QuoteView, self).get_context_data(**kwargs)
         context['page_list']=Page.objects.all()
         return context
-
+@login_required(login_url=reverse_lazy('login'))
 def quote_req(request):
     submitted = False
     if request.method == 'POST':
         form = QuoteForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            quote = form.save(commit=False)
+            try:
+                quote.username = request.user
+            except Exception:
+                pass
+            quote.save()
+            #form.save()
             return HttpResponseRedirect('/quote?submitted=True')
     else:
         form = QuoteForm()
